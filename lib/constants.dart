4892e8505006cc7a725713a9cc97acb7dd4b128b
Ahmed Gad <agad92998@example.com>
2023-12-18 21:42:42 +0200

    Article Done

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/services.dart';
 
 const String kViewType = "video_player_view";
-const Map<String, dynamic> kCreationParams = <String, dynamic>{
-  'videoUrl': "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
-};
+const Map<String, dynamic> kCreationParams = <String, dynamic>{};
 const MethodChannel kPlatformChannel = MethodChannel('video_player_channel');
--- lib/video_player_view.dart
+++ lib/video_player_view.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 
 import 'constants.dart';
@@ -15,30 +12,46 @@ class VideoPlayerView extends StatefulWidget {
 
 class _VideoPlayerViewState extends State<VideoPlayerView> {
   @override
+  void initState() {
+    super.initState();
+    kCreationParams['url'] =
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4";
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PlatformViewLink(
-        surfaceFactory: (context, controller) {
-          return AndroidViewSurface(
-            controller: controller as AndroidViewController,
-            hitTestBehavior: PlatformViewHitTestBehavior.opaque,
-            gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
-          );
-        },
-        onCreatePlatformView: (PlatformViewCreationParams params) {
-          return PlatformViewsService.initAndroidView(
-            id: params.id,
-            viewType: kViewType,
-            layoutDirection: TextDirection.ltr,
-            creationParams: kCreationParams,
-            creationParamsCodec: const StandardMessageCodec(),
-            onFocus: () {
-              params.onFocusChanged(true);
-            },
-          )..addOnPlatformViewCreatedListener(params.onPlatformViewCreated)..create();
-        },
-        viewType: kViewType,
-      ),
+    return const AndroidView(
+      viewType: kViewType,
+      layoutDirection: TextDirection.ltr,
+      creationParams: kCreationParams,
+      creationParamsCodec: StandardMessageCodec(),
     );
+
+    // Scaffold(
+    //   body: PlatformViewLink(
+    //     surfaceFactory: (context, controller) {
+    //       return AndroidViewSurface(
+    //         controller: controller as AndroidViewController,
+    //         hitTestBehavior: PlatformViewHitTestBehavior.opaque,
+    //         gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
+    //       );
+    //     },
+    //     onCreatePlatformView: (PlatformViewCreationParams params) {
+    //       return PlatformViewsService.initAndroidView(
+    //         id: params.id,
+    //         viewType: kViewType,
+    //         layoutDirection: TextDirection.ltr,
+    //         creationParams: kCreationParams,
+    //         creationParamsCodec: const StandardMessageCodec(),
+    //         onFocus: () {
+    //           params.onFocusChanged(true);
+    //         },
+    //       )
+    //         ..addOnPlatformViewCreatedListener(params.onPlatformViewCreated)
+    //         ..create();
+    //     },
+    //     viewType: kViewType,
+    //   ),
+    // );
   }
 }
